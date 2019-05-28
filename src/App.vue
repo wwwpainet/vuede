059a104e82fa62eafef46a64cb2ec6d57b45489d
wwwpai <xyqt888@gmail.com>
2019-05-29 00:39:05 +0800

    组件调用登录接口

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
+      <svg-icon icon-name="spades" width="24" height="24" icon-color="red"></svg-icon>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import {login} from 'api/login'
+
+export default {
+  created() {
+    let username = 'root'
+    let password = '123456'
+    login(username, password).then(() => {
+      // console.log(res)
+    })
+    console.log('测试 ')
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
